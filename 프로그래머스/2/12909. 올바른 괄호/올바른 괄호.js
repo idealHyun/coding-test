a553343fec6c15102ffa392a9acdf9dea4fafb3e
SanghyunLee <118160647+idealHyun@users.noreply.github.com>
2024-06-15 14:44:51 +0900

    [level 2] Title: 올바른 괄호, Time: 2.59 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,16 @@
+function solution(s){
+    let a=0;
+
+    for(let i=0;i<s.length;i++){
+        if(s[i]==='('){
+            a++;
+        }
+        else{
+            a--;
+            if(a<0)
+                break;
+        }
+    }
+
+    return a===0 ? true : false;
+}
