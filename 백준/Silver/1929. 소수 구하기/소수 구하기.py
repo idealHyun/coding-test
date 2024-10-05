b27175638342d1ab02bc72b6bf93b02e2aac8a71
SanghyunLee <118160647+idealHyun@users.noreply.github.com>
2024-10-05 14:32:33 +0900

    [Silver III] Title: 소수 구하기, Time: 4900 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,16 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+M,N = map(int,input().split())
+answer=[]
+
+for i in range(M,N+1):
+    if i==1:
+        continue
+    flag=True
+    for j in range(2,int(i ** 0.5) + 1):
+        if i%j==0:
+            flag=False
+            break
+    if flag:
+        print(i)  
