ffa4fb1102d42ad38f5d4270c815fa2a0a5859fe
SanghyunLee <118160647+idealHyun@users.noreply.github.com>
2024-10-18 19:45:06 +0900

    [level 2] Title: 스킬트리, Time: 0.02 ms, Memory: 9.99 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/README.md
+++ 프로그래머스/2/49993. 스킬트리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 스킬트리 - 49993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993#fn1) 
 
 ### 성능 요약
 
-메모리: 10 MB, 시간: 0.01 ms
+메모리: 9.99 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 18일 19:02:54
+2024년 10월 18일 19:45:03
 
 ### 문제 설명
 
--- 프로그래머스/2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -2,15 +2,11 @@ def solution(skill, skill_trees):
     answer = 0
 
     for skill_tree in skill_trees:
-        skill_arr = list(skill)
-        skill_arr.reverse()
-        flag=True
+        skill_arr = list(skill)[::-1]
         for s in skill_tree:
-            if s in skill_arr:
-                if s!=skill_arr.pop():
-                    flag=False
-                    break
-        if flag:
+            if s in skill_arr and s!=skill_arr.pop():
+                break
+        else:
             answer+=1          
         
     return answer
