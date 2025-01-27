7c00309a629856782998c64356c0527655586e73
SanghyunLee <lsh9736@gmail.com>
2025-01-28 01:11:58 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.05 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,16 +1,14 @@
 class Solution {
     public String solution(String phone_number) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
         int starCount = phone_number.length() - 4;
-        for (int i=0;i<phone_number.length();i++){
-            if(i<starCount){
-                answer+= '*';
-            }
-            else{
-                answer += phone_number.charAt(i);
-            }
+
+        for (int i = 0; i < starCount; i++) {
+            answer.append('*');
         }
-        
-        return answer;
+
+        answer.append(phone_number.substring(starCount));
+
+        return answer.toString();
     }
 }
