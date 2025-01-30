1c17a95647a61050e3a90fea8ab921dfddea481d
SanghyunLee <lsh9736@gmail.com>
2025-01-31 08:51:10 +0900

    [level 2] Title: 올바른 괄호, Time: 14.45 ms, Memory: 54.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Deque<Character> stack = new ArrayDeque<>();
+        
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                stack.push('(');
+            }
+            else if(s.charAt(i)==')') {
+                if(stack.isEmpty() || stack.pop()!='('){
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
