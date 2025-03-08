cfc732f611029a0ec82f87f314b6b11570bb27c8
SanghyunLee <lsh9736@gmail.com>
2025-03-08 14:06:15 +0900

    [Gold IV] Title: 수 묶기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.py
+++ 백준/Gold/1744. 수 묶기/수 묶기.py
@@ -5,35 +5,39 @@ N = int(input())
 num_arr = [int(input()) for _ in range(N)]
 answer = 0
 
-# 배열 정렬
+# 배열 내림차순 정렬
 num_arr.sort(reverse=True)
-split_index = N
 
+# 0 이하 숫자의 첫 번째 인덱스 찾기
+split_index = N
 for i in range(N):
   if num_arr[i] <=0:
     split_index = i
     break
 
-# 양수 
+# 양수 배열과 음수 + 0 배열 분리
 plus_arr = num_arr[0:split_index]
+minus_arr = sorted(num_arr[split_index:])
+
+# 양수 배열에서 1이 있는 경우 처리
+# 1은 양수랑 곱하는 것보다 더하는게 결과값 최대화
 if 1 in plus_arr:
   one_index = num_arr.index(1)
   one_arr = plus_arr[one_index:]
   plus_arr = plus_arr[:one_index]
+  # 1의 개수 만큼 미리 결과 값에 더하기
   answer += len(one_arr)  
 
-minus_arr = sorted(num_arr[split_index:])
+def sum_arr(arr):
+  len_arr = len(arr)
+  total = 0
+  # 앞에서 부터 2개씩 묶어서 더하기
+  for i in range(0,len_arr-1,2):
+    total += arr[i] * arr[i+1]
+  # 1개 남았을 경우 더하기
+  if len_arr % 2 ==1 :
+    total += arr[len_arr-1]
 
-for i in range(len(plus_arr))[::2]:
-  if i + 1 < len(plus_arr):
-    answer += plus_arr[i] * plus_arr[i+1]
-  else:
-    answer +=plus_arr[i]
+  return total
 
-for i in range(len(minus_arr))[::2]:
-  if i + 1 < len(minus_arr):
-     answer += minus_arr[i] * minus_arr[i+1]
-  else:
-    answer +=minus_arr[i]
-
-print(answer)
+print(answer + sum_arr(plus_arr) + sum_arr(minus_arr))
